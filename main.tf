085d540f46f4a37d97e86e707b86d9c2128a46c2
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-12-30 15:11:04 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 module "my_workerpool" {
-  source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v2.3.1"
+  source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v2.6.4"
 
   configuration = <<-EOT
     export SPACELIFT_TOKEN="${var.worker_pool_config}"
